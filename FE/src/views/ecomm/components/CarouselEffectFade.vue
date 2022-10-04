64a2b716a1e935dac3cff2b87508ca7a950af644
carlxaeron <carlxaeron09@gmail.com>
2022-10-04 20:41:08 +0800

    add carousel and adjust based on mockup

---
+++ FE/src/views/ecomm/components/CarouselEffectFade.vue
@@ -0,0 +1,61 @@
+<!-- =========================================================================================
+  File Name: CarouselEffectFade.vue
+  Description: Carousel with fade effect
+  ----------------------------------------------------------------------------------------
+  Item Name: Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template
+  Author: Pixinvent
+  Author URL: http://www.themeforest.net/user/pixinvent
+========================================================================================== -->
+
+
+<template>
+        <!-- swiper -->
+        <swiper :options="swiperOption" :dir="$vs.rtl ? 'rtl' : 'ltr'" :key="$vs.rtl">
+            <swiper-slide>
+              <img class="responsive" src="@/assets/images/pages/carousel/banner-16.jpg" alt="banner">
+            </swiper-slide>
+            <swiper-slide>
+              <img class="responsive" src="@/assets/images/pages/carousel/banner-10.jpg" alt="banner">
+            </swiper-slide>
+            <swiper-slide>
+              <img class="responsive" src="@/assets/images/pages/carousel/banner-15.jpg" alt="banner">
+            </swiper-slide>
+            <swiper-slide>
+              <img class="responsive" src="@/assets/images/pages/carousel/banner-18.jpg" alt="banner">
+            </swiper-slide>
+            <!-- <div class="swiper-pagination swiper-pagination-white" slot="pagination"></div>
+            <div class="swiper-button-prev swiper-button-white" slot="button-prev"></div>
+            <div class="swiper-button-next swiper-button-white" slot="button-next"></div> -->
+        </swiper>
+</template>
+
+<script>
+import 'swiper/dist/css/swiper.min.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+export default {
+  data () {
+    return {
+      swiperOption: {
+        spaceBetween: 30,
+        effect: 'fade',
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        autoplay: {
+          delay: 3000
+        }
+      }
+    }
+  },
+  components: {
+    swiper,
+    swiperSlide
+  }
+}
+</script>
